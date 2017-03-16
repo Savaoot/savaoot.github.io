201cea0ae89af7ac79a3eef1f38aa8c4d0c04f27
Goran Ninković <warhawk@hotmail.rs>
2017-03-16 22:28:09 +0000

    Uradjen fadeIn na headeru kad se ucita stranica

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,53 +1,59 @@
 $(document).ready(function() {
-   
+    // $(this).css("display", "block");
+    // $("body header nav div.row").slideDown(1000);
+
+    $(".js--row").css("display", "none");
+    $("body header nav div.row").fadeIn(1000, function(e) {
+        $(this).css("display", "block");
+    });
+
     $('.js--section-who-we-are').waypoint(function(direction) {
-        
+
         if (direction == "down") {
-           $('nav').addClass('sticky'); 
-        } else {
+            $('nav').addClass('sticky');
+        }
+        else {
             $('nav').removeClass('sticky');
         }
-        
+
     }, {
         offset: '600px'
     });
-    
+
     /*----nav-scrolling--- */
-    
-    
     $(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
-    
-/*------------js for animations------------- */
- $('.js--wp-1').waypoint(function(direction) {
-     $('.js--wp-1').addClass(' animated fadeInLeft');
- }, {
-     offset: '55%'
- });
-    
-     $('.js--wp-2').waypoint(function(direction) {
-      $('.js--wp-2').addClass(' animated fadeInUpBig');
- }, {
-     offset: '55%'
- });
-    
-    
-     $('.js--wp-4').waypoint(function(direction) {
-      $('.js--wp-4').addClass(' animated zoomIn');
- }, {
-     offset: '55%'
- });
-    
+        $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
+    /*------------js for animations------------- */
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass(' animated fadeInLeft');
+    }, {
+        offset: '55%'
+    });
+
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass(' animated fadeInUpBig');
+    }, {
+        offset: '55%'
+    });
+
+
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass(' animated zoomIn');
+    }, {
+        offset: '55%'
+    });
+
 });
