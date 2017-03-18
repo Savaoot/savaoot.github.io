c5dead43d27d8863418c6253bcad8655f86dfe39
TheEscape94 <maperlic@gmail.com>
2017-03-18 03:30:53 +0100

    mala izmena animacija
    svi h1 tagovi imaju animaciju zoomIn

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // $(this).css("display", "block");
     // $("body header nav div.row").slideDown(1000);
 
@@ -7,11 +7,11 @@ $(document).ready(function() {
     setTimeout(showRightDiv, 4500);
 
     $("header nav div.row").css("display", "none");
-    $("header nav div.row").fadeIn(1000, function(e) {
+    $("header nav div.row").fadeIn(1000, function (e) {
         $(this).css("display", "block");
     });
 
-    $('.js--section-who-we-are').waypoint(function(direction) {
+    $('.js--section-who-we-are').waypoint(function (direction) {
 
         if (direction == "down") {
             $('nav').addClass('sticky');
@@ -25,8 +25,8 @@ $(document).ready(function() {
     });
 
     /*----nav-scrolling--- */
-    $(function() {
-        $('a[href*="#"]:not([href="#"])').click(function() {
+    $(function () {
+        $('a[href*="#"]:not([href="#"])').click(function () {
             if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
                 var target = $(this.hash);
                 target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -42,50 +42,70 @@ $(document).ready(function() {
 
     /*------------js for animations------------- */
 
-    $('.js--wp-1').waypoint(function(direction) {
-        $('.js--wp-1').addClass(' animated fadeInLeft');
+    $('.js--wp-1').waypoint(function (direction) {
+        $('.js--wp-1').addClass(' animated zoomIn');
     }, {
         offset: '55%'
     });
 
-    $('.js--wp-2').waypoint(function(direction) {
-        $('.js--wp-2').addClass(' animated fadeInUpBig');
+    $('.js--wp-2').waypoint(function (direction) {
+        $('.js--wp-2').addClass(' animated zoomIn');
+    }, {
+        offset: '55%'
+    });
+
+    $('.js--wp-3').waypoint(function (direction) {
+        $('.js--wp-3').addClass(' animated zoomIn');
     }, {
         offset: '55%'
     });
 
 
-    $('.js--wp-4').waypoint(function(direction) {
+    $('.js--wp-4').waypoint(function (direction) {
         $('.js--wp-4').addClass(' animated zoomIn');
     }, {
         offset: '55%'
     });
+
+    $('.js--wp-5').waypoint(function (direction) {
+        $('.js--wp-2').addClass(' animated zoomIn');
+    }, {
+        offset: '55%'
+    });
+
+    $('.js--wp-6').waypoint(function (direction) {
+        $('.js--wp-6').addClass(' animated zoomIn');
+    }, {
+        offset: '55%'
+    });
+
+
 
 });
 //header functions
 
 //id: lph-1, lph-2, lph-3, lph-4
 function showLeftDiv() {
-    setTimeout(function() {
+    setTimeout(function () {
         $("#lph-1").css("display", "block");
         $("#lph-1").addClass("animated bounceInDown");
     }, 0);
-    setTimeout(function() {
+    setTimeout(function () {
         $("#lph-2").css("display", "block");
         $("#lph-2").addClass("animated bounceInDown");
     }, 600);
-    setTimeout(function() {
+    setTimeout(function () {
         $("#lph-3").css("display", "block");
         $("#lph-3").addClass("animated bounceInDown");
     }, 1200);
-    setTimeout(function() {
+    setTimeout(function () {
         $("#lph-4").css("display", "block");
         $("#lph-4").addClass("animated bounceInDown");
     }, 1800);
 }
 
 function showRightDiv() {
-    setInterval(function() {
+    setInterval(function () {
         $("#right_part").css("display", "block");
         $("#right_part").addClass("animated fadeInDown");
     }, 2400);
@@ -93,7 +113,7 @@ function showRightDiv() {
 
 //id: first_screen_p, second_screen_p
 function showScreenDiv() {
-    setTimeout(function() {
+    setTimeout(function () {
         $("#first_screen_p").css("display", "block");
     }, 500);
 }
